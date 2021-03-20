9f35ae9c3a070d162d3cdbc2f27c53170842a9c3
oka884 <rrrec.nyossbk@gmail.com>
2021-03-20 15:18:08 +0900

    ひとまず完成

--- view/PN202narabeFinishView.php
+++ view/PN202narabeFinishView.php
@@ -11,6 +11,7 @@
 
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<link rel="stylesheet" href="../view/narabeCss/reset.css" type="text/css">
 	<link rel="stylesheet" href="../view/narabeCss/common.css" type="text/css">
 	<link rel="stylesheet" href="../view/narabeCss/narabe.css" type="text/css">
